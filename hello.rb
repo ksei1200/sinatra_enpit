746f641861047929aa79af449f77a099aee4137e
Seiichi <skawashi@cisco.com>
2018-07-06 10:00:41 +0000

    modify hello.rb

--- hello.rb
+++ hello.rb
@@ -1,30 +1,7 @@
 require 'sinatra'
-require 'sinatra/reloader'
 
-before do
-  @profile_link = :'/user/profile'
-  @help_link = :'/user/help'
+get '/' do
+  "Hello World!"
+  " This is Team HKKM Home Page "
+
 end
-
-get '/index' do
-  erb :index
-end
-
-get '/:user/profile' do
-  @name = 'Profile'
-  erb '/user/profile'.to_sym
-end
-
-get '/:user/help' do
-  @users = ['Sally','Jerry','Rocko']
-  erb :'/user/help'
-end
-
-#get '/hi' do
-#  "Hello World!"
-#end
-
-#get '/' do
-#  pass if Time.now > time_for('Dec 23, 2018')
-#  "まだ時間がある"
-#end
