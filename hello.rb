67ea291264f678c626bdf2f0dadf18de254f8ad4
Seiichi <skawashi@cisco.com>
2018-07-06 09:46:43 +0000

    edit hello.rb

--- hello.rb
+++ hello.rb
@@ -1,4 +1,26 @@
 require 'sinatra'
+require 'sinatra/reloader'
+
+before do
+  @profile_link = :'/user/profile'
+  @help_link = :'/user/help'
+end
+
+get '/index' do
+  erb :index
+end
+
+get '/:user/profile' do
+  @name = 'Profile'
+  erb '/user/profile'.to_sym
+end
+
+get '/:user/help' do
+  @users = ['Sally','Jerry','Rocko']
+  erb :'/user/help'
+end
+
+require 'sinatra'
 
 get '/hi' do
   "Hello World!"
